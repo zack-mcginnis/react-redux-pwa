c9f0be6260b303330ee294d0f7d6214bfd5ccc2e
zack <zackmcginnis@hotmail.com>
2020-01-20 09:39:11 -0800

    new create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import logo from './logo.svg'
-import './App.css'
-import store from './store'
-import Customers from './components/Customer/customers'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-
-  render () {
-    return (
-      <Provider store={ store }>
-        <div className="App">
-          <header className="App-header">
-            <img src={ logo } className="App-logo" alt="logo"/>
-            <h1 className="App-title">React/Redux Express Starter</h1>
-          </header>
-          <Customers/>
-        </div>
-      </Provider>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
