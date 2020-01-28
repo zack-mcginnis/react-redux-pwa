4f2ba48e51ffe96689fa3f0e6312eb6f448f2d52
zack <zackmcginnis@hotmail.com>
2020-01-27 20:04:40 -0800

    updates to ui

--- client/src/components/Address/address.js
+++ client/src/components/Address/address.js
@@ -8,8 +8,13 @@ class Address extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: ''
+      address: '',
+      header: 'Enter a BTC address to verify it has not been used',
+      result: '',
+      error: false
     };
+
+    this.displayResult = this.displayResult.bind(this);
   }
 
   static propTypes = {
@@ -23,21 +28,61 @@ class Address extends Component {
     this.setState({ address: e.target.value });
   }
 
-  handleClick(){
+  async handleClick(){
     console.log(this.state.address)
-    this.props.getAddressReceiveHistory(this.state.address)
+    try {
+      const result = await this.props.getAddressReceiveHistory(this.state.address)
+
+      if(result.payload.reason) {
+        this.setState({error: true})
+      } else {
+        this.setState({result: result.payload})
+      }
+
+      console.log(result)
+    } catch (err) {
+      console.log(err)
+      this.setState({error: true})
+    }
+
+    this.displayResult()
+  }
+
+  displayResult() {
+    if(this.state.error) {
+      return `Invalid address`
+    }
+
+    // if we don't have a result yet
+    if(this.state.result === '') {
+      return `Enter a valid BTC address`
+    }
+
+    // if the address has received btc in the past
+    // test: 1EnJHhq8Jq8vDuZA5ahVh6H4t6jh1mB4rq
+    if(this.state.result > 0) {
+      return `This address has received ${this.state.result} satoshis`
+    } else {
+      // if the address has not received btc yet
+      // test: 3MsUbTHEAgvgVgRgpR8fCbS27wjZyHJPwr
+      return `This address has not received BTC yet`
+    }
   }
 
   render() {
 
     return (
       <div>
+        <h3>{this.state.header}</h3>
         <input type="text" onChange={ (e) => this.handleChange(e) } />
         <input
           type="button"
-          value="Alert the text input"
+          value="Submit"
           onClick={ (e) => this.handleClick(e)}
         />
+        <div>
+          {this.displayResult()}
+        </div>
       </div>
     );
   }
